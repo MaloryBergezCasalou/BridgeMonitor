2c3c2633fcc75df9203bffc1e97fb6c6429cd1c3
malorybergezcasalou <malory.bergezcasalou@ynov.com>
2021-04-03 22:50:15 +0200

    page accueil

--- BridgeMonitor/BridgeMonitor/Controllers/HomeController.cs
+++ BridgeMonitor/BridgeMonitor/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using BridgeMonitor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace BridgeMonitor.Controllers
@@ -20,18 +22,41 @@ namespace BridgeMonitor.Controllers
 
         public IActionResult Index()
         {
+            List<BoatModel> apiInfo = GetFromAPI();
+            apiInfo.Sort((s1, s2) => DateTimeOffset.Compare(s1.ClosingDate, s2.ClosingDate));
+
+            foreach (var close in apiInfo)
+            {
+                if (DateTimeOffset.Compare(DateTimeOffset.Now, close.ClosingDate) < 0)
+                {
+                    ViewData["Info0"] = close;
+                    break;
+                }
+            }
+
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult AllClose()
         {
+            ViewData["Info1"] = GetFromAPI();
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public static List<BoatModel> GetFromAPI()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php");
+                var stringResult = response.Result.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<List<BoatModel>>(stringResult.Result);
+                return result;
+            }
         }
     }
 }
---
+++ BridgeMonitor/BridgeMonitor/Models/BoatModel.cs
@@ -0,0 +1,21 @@
+using Newtonsoft.Json;
+using System;
+using System.Runtime.InteropServices;
+
+namespace BridgeMonitor.Models
+{
+    public class BoatModel
+    {
+        [JsonProperty("boat_name")]
+        public string BoatName { get; set; }
+
+        [JsonProperty("closing_type")]
+        public string ClosingType { get; set; }
+
+        [JsonProperty("closing_date")]
+        public DateTime ClosingDate { get; set; }
+
+        [JsonProperty("reopening_date")]
+        public DateTime ReopeningDate { get; set; }
+    }
+}
